3bcd83c5847817bc9b5dc5c4a003324228bc5e64
mantis-gdn <info@gigdates.net>
2026-01-19 22:30:02 -0500

    feat: remove players 11 and 12 from multiplayer game configuration

--- src/main.js
+++ src/main.js
@@ -70,7 +70,7 @@ camera.lookAt(0, cube.position.y, 0);
 // Multiplayer Game
 // --------------------
 const game = createMultiPlayerGame({
-  players: ["P1", "P2", "P3", "P4", "P5", "P6", "P7", "P8", "P9", "P10", "P11", "P12"],
+  players: ["P1", "P2", "P3", "P4", "P5", "P6", "P7", "P8", "P9", "P10"],
   startingBankroll: 200,
   startingMinBet: 10,
   minBetIncrease: 5,
